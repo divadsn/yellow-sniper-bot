cc9711435d6ce82b2bc7da9426bb508637f34892
David Sn <divad.nnamtdeis@gmail.com>
2025-02-09 17:52:54 +0100

    feat(bot): Auto-stop the bot when all slots have been reserved

--- glovobot/bot.py
+++ glovobot/bot.py
@@ -14,6 +14,9 @@ from glovobot.client import GlovoAPIClient
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger("glovobot")
 
+# Disable httpx logging
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
 
 class GlovoBot:
     def __init__(self, client: GlovoAPIClient = None) -> None:
@@ -32,7 +35,7 @@ class GlovoBot:
                     await self._client.refresh_token()
                 except httpx.HTTPError as e:
                     logger.error(f"Failed to refresh token", exc_info=e)
-                    self.send_webhook_message(f"❌ Failed to refresh token:\n`{e}`")
+                    await self.send_webhook_message(f"❌ Failed to refresh token:\n`{e}`")
                     break
 
                 self._client.save("device.json")
@@ -40,32 +43,48 @@ class GlovoBot:
             logger.info(f"Token valid until {datetime.fromtimestamp(jwt_token['exp'], tz=timezone.utc)}")
 
             try:
-                await self.check_slots()
+                if await self.check_slots():
+                    logger.info("All required slots have been reserved, stopping...")
+                    await self.send_webhook_message("🎉 All required slots have been reserved!")
+                    break
             except Exception as e:
                 logger.error(f"An error occurred while checking slots", exc_info=e)
-                self.send_webhook_message(f"❌ An error occurred while checking slots:\n`{e}`")
+                await self.send_webhook_message(f"❌ An error occurred while checking slots:\n`{e}`")
 
             await asyncio.sleep(CHECK_INTERVAL + random.randint(0, 10000) / 1000)
 
 
-    async def check_slots(self) -> None:
+    async def check_slots(self) -> bool:
         logger.info("Pulling available calendar data from Glovo API")
+        self._calendar = await self._client.get_calendar()
 
-        calendar = await self._client.get_calendar()        
-        booked_slots = []
+        booked_slots = []  # List of newly booked slots
+        target_slots = 0  # Count total relevant slots (from BOOKING_HOURS)
+        booked_target_slots = 0  # Count how many are already booked
 
-        for day in calendar["days"]:
+        for day in self._calendar["days"]:
             current_date = datetime.fromtimestamp(day["date"], tz=timezone.utc)
             logger.info(f"Checking available slots for {current_date:%A, %d %B %Y}")
 
             for zone in day["zonesSchedule"]:
+                zone_booking_hours = BOOKING_HOURS.get(zone["name"])
+
+                if not zone_booking_hours:
+                    continue
+
                 for slot in zone["slots"]:
                     start_time = slot["startTimeFormatted"]
 
-                    if slot["status"] != "AVAILABLE":
+                    if start_time not in zone_booking_hours.get(day["name"], []):
                         continue
 
-                    if start_time not in BOOKING_HOURS.get(day["name"], []):
+                    target_slots += 1
+
+                    if slot["status"] == "BOOKED":
+                        booked_target_slots += 1
+                        continue
+
+                    if slot["status"] != "AVAILABLE":
                         continue
 
                     if not BOOK_NO_BOOST and not any(tag_type == "RUSH" for tag_type in slot["tags"]["types"]):
@@ -81,8 +100,8 @@ class GlovoBot:
                         continue
 
                     logger.info(f"Slot {slot['id']} on booked successfully")
-                    self._last_slot_id = slot["id"]
 
+                    booked_target_slots += 1
                     booked_slots.append({
                         "id": slot["id"],
                         "date": current_date.strftime("%a, %d %B %Y"),
@@ -96,7 +115,8 @@ class GlovoBot:
             message += "\n".join([f"- {slot['id']}: *{slot['date']}* at *{slot['start_time']}*" for slot in booked_slots])
             await self.send_webhook_message(message)
 
-        self._calendar = calendar
+        logger.info(f"Booked target slots: {booked_target_slots}/{target_slots}")
+        return target_slots > 0 and target_slots == booked_target_slots
 
     async def send_webhook_message(self, message: str) -> None:
         try:
